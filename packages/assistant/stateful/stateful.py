c56c3d3a88995615f2fdae0098d7e701c9821d23
GrisafiAlessandro <grisafialessandro@gmail.com>
2025-03-16 11:14:16 +0000

    lesson 4

---
+++ packages/assistant/stateful/stateful.py
@@ -0,0 +1,29 @@
+import chat, history
+
+def stateful(args):
+  
+  inp = args.get("input", "")
+  out = f"Hello from {chat.MODEL}"
+  res = {}
+  
+  if inp != "":
+    # load the history in the chat
+    ch = chat.Chat(args)
+    #TODO:E4.2 load the history
+    hi = history.History(args)
+    hi.load(ch)
+    #END TODO
+    # add a message and save it 
+    msg = f"user:{inp}"
+    ch.add(msg)
+    print(ch.messages)
+    out = ch.complete()
+    # complete, save the assistant and return the id
+    #TODO:E4.2 save the message and the state
+    # return the id as state field in the response
+    hi.save(f"assistant:{out}")
+    res['state'] = hi.id()
+    #END TODO
+
+  res['output'] = out
+  return res
